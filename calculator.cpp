c0e0e8c0af0da8085055066b49aa9283225c015d
Ne1s07 <ne1s07@outlook.com>
2018-07-07 17:27:02 +0800

    用分数重写程序。

--- calculator.cpp
+++ calculator.cpp
@@ -11,11 +11,11 @@ string basic_calculate(size_t begin, size_t count, string str);
 string bracket_process(size_t begin, size_t count, string str);
 
 void print_error(string error);
-void verify(long double d);
+void verify(const Fraction d);
 
 ostringstream sstream;		//将double转为string的流。
 
-inline long double get_operands(const string str, const size_t found, long double &right, size_t &length, size_t &st_left);
+inline Fraction get_operands(const string str, const size_t found, Fraction &right, size_t &length, size_t &st_left);
 
 int main(){
 	sstream.precision(std::numeric_limits<double>::digits10);
@@ -39,62 +39,66 @@ string basic_calculate(size_t begin, size_t count, string str){
 	size_t found;				//运算符号位置。
 	size_t length;				//计算对象长度。
 	size_t st_left, st_right;	//执行运算部分的起始位置。
-	long double left, right, result;	//运算符左右侧数字以及计算结果。
+	Fraction left, right, result;	//运算符左右侧数字以及计算结果。
 	
 
 	//执行三级运算(幂运算)。
-	found = cal_str.find_first_of("^");
+	found = cal_str.find_first_of("^!");
 	while(found != string::npos){
 		left = get_operands(cal_str, found, right, length, st_left);
+		if(cal_str[found] == '^'){
+			result = left;
+			if(right.n == 0){
+				result.n = 1;
+				result.d = 1;
+			}
+			else if(right.n > 0){
+				while(--right.n != 0){
+					result = result * left;
+				}
+			}
+			else if(right.n < 0){
+				while(++right.n != 0){
+					result = result * left;
+				}
+				result = Fraction() / result;
+			}
+		}
+		else if(cal_str[found] == '!'){
+			if((found+1) != cal_str.size() && cal_str[found + 1] == '!'){		//判断是否为双阶乘
 
-		result = left;
-		if(right == 0){
-			result = 1;
-		}
-		else if(right > 0){
-			while(--right != 0){
-				result *= left;
+			}
+			else{
+				
 			}
 		}
-		else if(right < 0){
-			while(++right != 0){
-				result *= left;
-			}
-			result = 1 / result;
-		}
+
 		verify(result);
 
 		sstream << result;
 		cal_str.replace(st_left, length, sstream.str());
 		sstream.str("");
-		found = cal_str.find_first_of("^");
+		found = cal_str.find_first_of("^!");
 	}
 
 	//执行二级运算(乘除运算)。
 	found = cal_str.find_first_of("*/");
 	while(found != string::npos){
-		st_left = found - 1;
-		st_right = found + 1;
+		left = get_operands(cal_str, found, right, length, st_left);
 		if(cal_str[found] == '*'){	//执行乘法运算。
-			left = get_operands(cal_str, found, right, length, st_left);
 			result = left * right;
-			verify(result);
 
-			//将结果(double)转为string。
-			sstream << result;
-			cal_str.replace(st_left, length, sstream.str());
-			sstream.str("");			//重置流。
 		}
 		else if(cal_str[found] == '/'){	//执行除法运算。
-			left = get_operands(cal_str, found, right, length, st_left);
 			result = left / right;
-			verify(result);
-			
-			sstream << result;
-			cal_str.replace(st_left, length, sstream.str());
-
-			sstream.str("");
 		}
+
+		verify(result);
+		//将结果(double)转为string。
+		sstream << result;
+		cal_str.replace(st_left, length, sstream.str());
+		sstream.str("");			//重置流。
+
 		found = cal_str.find_first_of("*/");
 	}
 
@@ -105,31 +109,27 @@ string basic_calculate(size_t begin, size_t count, string str){
 				found = cal_str.find_first_of("+-", found + 1);
 				continue;
 		}
-		if(cal_str[found] == '+'){
-			left = get_operands(cal_str, found, right, length, st_left);
-			result = left + right;
-			verify(result);
+		left = get_operands(cal_str, found, right, length, st_left);
 
-			sstream << result;
-			cal_str.replace(st_left, length, sstream.str());
-			cout << "cal_str: " << cal_str << endl;
-			sstream.str("");
+		if(cal_str[found] == '+'){
+			result = left + right;
 		}
 		else if(cal_str[found] == '-'){
 			if((found - 1 >= 0) && isdigit(cal_str[found - 1])){
-				left = get_operands(cal_str, found, right, length, st_left);
 				result = left - right;
-				verify(result);
 
-				sstream << result;
-				cal_str.replace(st_left, length, sstream.str());
-				sstream.str("");
 			}
 			else{
 				found = cal_str.find_first_of("+-", found + 1);
 				continue;
 			}
 		}
+
+		verify(result);
+		sstream << result;
+		cal_str.replace(st_left, length, sstream.str());
+		sstream.str("");
+
 		found = cal_str.find_first_of("+-");
 	}
 	return cal_str;
@@ -160,47 +160,40 @@ string bracket_process(size_t begin, size_t count, string str){
 }
 
 
-inline long double get_operands(const string str, const size_t found, long double &right, size_t &length, size_t &st_left){
-	long double left;
+inline Fraction get_operands(const string str, const size_t found, Fraction &right, size_t &length, size_t &st_left){
+	Fraction left;
 	st_left = found - 1;
 	size_t st_right = found + 1;
-
+	string operands= "+-*/^";
 	while(st_left != 0){
-		if(isdigit(str[st_left]) || str[st_left] == '.'){
+		if(operands.find(str[st_left]) == string::npos){
 			--st_left;
 			continue;
 		}
-	 	if(str[st_left] == '-' && st_left - 1 != 0 && !isdigit(str[st_left - 1])){
-	 		--st_left;
-	 		continue;
-	 	}
-	 	++st_left;
 	 	break;
 	 }
 
 	while(st_right != str.size()){
-		if (isdigit(str[st_right]) || str[st_right] == '.'){
+		if (operands.find(str[st_right]) == string::npos){
 			++st_right;
 	 		continue;
 		}
-		if(str[st_right] == '-' && !isdigit(str[st_right - 1])){
-			++st_right;
-			continue;
-		}
-		--st_right;
 		break;
 	}
 
-	 
 	try{
-		right = stod(str.substr(found + 1));
-		left = stod(str.substr(st_left));
+		left = Fraction(str.substr(st_left, found - st_left));
+		right = Fraction(str.substr(found + 1, st_right - found));
 	}
 	catch(invalid_argument){
-		print_error("INVALID INPUT");
+		if(str[found] == '!'){
+
+		}
+		else{
+			print_error("INVALID INPUT");
+		}
 	}
-	
-	length = st_right - st_left + 1;
+	length = st_right - st_left;
 	return left;
 }
 
@@ -208,7 +201,7 @@ void print_error(const string error){
 	cerr << "\n---WARNING---\n" << error << endl;
 }
 
-void verify(const long double d){
-	if(d + 100 == d)
-		print_error("OVERFLOW\n");
+void verify(const Fraction d){
+	// if(d + 100 == d)
+	// 	print_error("OVERFLOW\n");
 }
--- fraction.h
+++ fraction.h
@@ -1,46 +1,121 @@
-#include <iostream>
 #ifndef FRACTION_H
 #define FRACTION_H
+#include <iostream>
+#include <string>
+#include <cmath>
+
+using namespace std;
 
 struct Fraction{
-	int n;		//分子。
-	int d;		//分母。
+	long long n = 1;		//分子。
+	long long d = 1;		//分母。
+	Fraction() = default;
+	Fraction(string str){
+		size_t found;
+		found = str.find('.');
+		if(found != string::npos){
+			d = pow(10, str.size() - found - 1);
+			str.erase(found, 1);
+			n = stoi(str);
+			reduce();
+			return;
+		}
+		found = str.find('|');
+		if(found != string::npos){
+			n = stoi(str);
+			cout << "alive" << endl;
+			d = stoi(str.substr(found + 1));
+			cout << "die" << endl;
+			return;
+		}
+		n = stoi(str);
+	}
 
-	Fraction& reduce(){
-		int cd = gcd(n, d);
+	Fraction& reduce(){			//约分。
+		long long cd = gcd(n, d);
 		n /= cd;
 		d /= cd;
 		return *this;
 	}
 
-	int gcd(int m, int n){
+	long long gcd(long long m, long long n){		//最大公因数。
 		if(n == 0)
 			return m;
 		return gcd(n, m%n);
 	}
 
-	int lcm(int m, int n){
+	long long lcm(long long m, long long n){		//最小公倍数。
 		return m * n / gcd(m, n);
 	}
 
-	void rtcd(Fraction &f1, Fraction &f2){
-		int a = lcm(f1.d, f2.d);
-		int f1_a = a / f1.d;
+	void rtcd(Fraction &f1, Fraction &f2){		//通分。
+		long long a = lcm(f1.d, f2.d);
+		long long f1_a = a / f1.d;
 		f1.d = a;
 		f1.n *= f1_a;
-		int f2_a = a / f2.d;
+		long long f2_a = a / f2.d;
 		f2.d = a;
 		f2.n *= f2_a;
 	}
 
-	Fraction &operator+(Fraction &f1, Fraction &f2){
-		rtcd(f1, f2);
+	ostream &operator<<(ostream &out);
+
+	Fraction operator+(const Fraction &f1){
+		Fraction fract = f1;
+		rtcd(fract, *this);
+		
 		Fraction result;
-		result.d = f1.d;
-		result.n = f1.n + f2.n;
+		result.d = fract.d;
+		result.n = fract.n + this->n;
+		result.reduce();
+		return result;
+	}
+
+	Fraction operator*(const Fraction &f1){
+		Fraction fract = f1;
+		Fraction result;
+
+		result.d = fract.d * this->d;
+		result.n = fract.n * this->n;
+		result.reduce();
+		return result;
+	}
+
+	Fraction operator/(const Fraction &f1){
+		Fraction fract;
+		fract.n = f1.d;
+		fract.d = f1.n;
+		
+		Fraction result;
+		result.d = fract.d * this->d;
+		result.n = fract.n * this->n;
+		result.reduce();
+		return result;
+	}
+
+	Fraction operator-(const Fraction &f1){
+		Fraction fract = f1;
+		rtcd(fract, *this);
+		
+		Fraction result;
+		result.d = fract.d;
+		result.n = fract.n - this->n;
 		result.reduce();
 		return result;
 	}
 };
+
+ostream &operator<<(ostream &out, Fraction &fract1){
+	if(fract1.d == 1)
+		out << to_string(fract1.n);
+	else{
+		Fraction f(to_string((double)(fract1.n) / (double)(fract1.d)));
+		if(f.n == fract1.n && f.d == fract1.d)
+			out << to_string((double)(fract1.n) / (double)(fract1.d));
+		else
+			out << to_string(fract1.n) << "|" << to_string(fract1.d);
+	}
+	return out;
+}
 
 #endif
